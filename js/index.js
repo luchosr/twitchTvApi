28b431d9e10f971366fcaee4e71b738b7d32ff90
Luciano Ramello <luchosr@gmail.com>
2017-06-22 21:31:42 -0300

    code improved

--- js/index.js
+++ js/index.js
@@ -3,9 +3,48 @@ console.log('funciona');
 $(document).ready(function(){
 var channel = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 var urlChannel = 'https://crossorig.in/https://wind-bow.glitch.me/twitch-api/streams/';
+var logo, display_name, status;
 for (var i = 0; i < channel.length; i++) {
- var channelUrli = urlChannel + channel[i];
-  console.log("la url queda: " + urlChannel + channel[i]);
+ urlChannel = urlChannel + channel[i];
+  console.log("la url queda: " + urlChannel);
+
+
+  $.ajax({
+  url: urlChannel,
+  method: 'GET',
+
+  success: function(data){
+    if(data.stream !== null){
+      console.log(data);
+     logo = data.stream.channel.logo;
+     console.log('el logo es: ' + data.stream.channel.logo);
+     display_name = data.stream.channel.display_name;
+     status = data.stream.channel.status;
+    $('.channels').append("<div><div class='col-md-4'><img src="+ logo +"></div><div class='col-md-4'>" + display_name+ "</div><div class='col-md-4'>" + status+ "</div></div>");
+  } else {
+    logo = "Image not found";
+
+    display_name = channel[i];
+    console.log(channel[i]);
+    console.log(display_name);
+    status = 'OFFLINE';
+   $('.channels').append("<div><div class='col-md-4'><img src="+ logo +"></div><div class='col-md-4'>" + display_name+ "</div><div class='col-md-4'>" + status+ "</div></div>");
+  }
+
+  // $('.channels').append("<div><div class='col-md-4'><img src="+ logo +"></div><div class='col-md-4'>" + display_name+ "</div><div class='col-md-4'>" + status+ "</div></div>");
+  // if (data.stream !== null) {
+  //
+  //   // $('.channels').append('appendea viteh!');
+  // }
+  // console.log(data);
+  }
+
+  });
+
+
+
+
+  urlChannel = 'https://crossorig.in/https://wind-bow.glitch.me/twitch-api/streams/';
 }
 });
 
